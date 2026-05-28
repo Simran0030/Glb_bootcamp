5ca8ad501602238f58dcd548563c4ac107006e65
simran singh <simran9594singh@gmail.com>
2026-05-28 23:32:22 +0530

    initial commit

---
+++ difference.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n; 
+    printf("enter the number");
+    scanf("%d",&n);
+    int arrr[50];
+    printf("enter the elements of the array");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arrr[i]);
+    }
+    for(int i=0;i<n;i++){
+        int diff =arrr[0]-arrr[n-1];
+        printf("%d ",diff);
+        break;
+    }
+    return 0;
+}
